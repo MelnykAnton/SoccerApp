b5593aed76ed2fc7758d375945bebedba0bba375
Arrepticius <barista.kh@gmail.com>
2017-05-18 16:34:32 +0300

    edited login

--- app/src/main/java/com/melnykanton/soccerapp/SignInActivity.java
+++ app/src/main/java/com/melnykanton/soccerapp/SignInActivity.java
@@ -4,6 +4,8 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -15,10 +17,20 @@ public class SignInActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private  FirebaseAuth.AuthStateListener mAuthListner;
 
+    private EditText email_input;
+    private EditText psw_input;
+
+    private Button login_button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        email_input = (EditText) findViewById(R.id.email_text_edit);
+        psw_input = (EditText) findViewById(R.id.psw_text_edit);
+
+        login_button = (Button) findViewById(R.id.login_button);
 
         mAuth = FirebaseAuth.getInstance();
 
