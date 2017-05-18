a09f473bcd22e2de7ecef605089e48ac511fecaa
Arrepticius <barista.kh@gmail.com>
2017-05-18 17:51:53 +0300

    edit SignIn

--- app/src/main/java/com/melnykanton/soccerapp/SignInActivity.java
+++ app/src/main/java/com/melnykanton/soccerapp/SignInActivity.java
@@ -3,14 +3,20 @@ package com.melnykanton.soccerapp;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class SignInActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener{
 
     private final String TAG = "Authenticate";
 
@@ -41,8 +47,9 @@ public class SignInActivity extends AppCompatActivity {
                 if (user != null) {
 
                     Log.d(TAG,"Authenticate successful");
+                }else {
+                    Log.d(TAG, "!!!Something goes wrong!!!");
                 }
-                Log.d(TAG,"!!!Something goes wrong!!!");
             }
         };
     }
@@ -59,5 +66,52 @@ public class SignInActivity extends AppCompatActivity {
         if (mAuthListner != null){
             mAuth.removeAuthStateListener(mAuthListner);
         }
+    }
+
+    public boolean validForm(){
+        boolean valid = false;
+
+        String mail = email_input.getText().toString();
+        if (TextUtils.isEmpty(mail)) {
+            email_input.setError("Required");
+            valid = false;
+        }else {
+            email_input.setError(null);
+        }
+
+        String psw = psw_input.getText().toString();
+        if (TextUtils.isEmpty(psw)) {
+            psw_input.setError("Required");
+            valid = false;
+        }else {
+            psw_input.setError(null);
+        }
+        return valid;
+    }
+
+    private void signIn(String mail, String pass) {
+        Log.d(TAG, "SignIn: " +mail);
+
+        if (validForm()) {
+            return;
+        }
+        mAuth.createUserWithEmailAndPassword(mail, pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG,"SignIn:onComplete: " + task.isSuccessful());
+
+                        if (!task.isSuccessful()){
+                            Log.d(TAG, "SignIn: failed" + task.getException());
+                            Toast.makeText(SignInActivity.this,"Filed",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
